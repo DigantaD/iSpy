fe35a97761164a443a6b4f8469209a23b6df58d1
Diganta Dutta <diggyhacker.coder@gmail.com>
2018-02-07 10:28:27 +0530

    Traces the ip address of the given network

---
+++ GetIP.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class GetIP extends Main {
+
+	String url="",url1="";
+	
+	public String Ip_Send() {
+
+		InetAddress address = null;
+		InetAddress address1 = null;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter URL/Network Address to be analyzed: \n1. With World Wide Web \n2. Without World Wide Web\n");
+		url = sc.next();
+		url1 = sc.next();
+
+		try {
+
+			address = InetAddress.getByName(url);
+			address1 = InetAddress.getByName(url1);
+		} catch (UnknownHostException e) {
+
+			System.out.println("Invalid or Unknown Address.");
+			System.exit(2);
+		}
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("/home/codebuster/ProjectLab_Trial/IP/IPv4.txt"))) {
+
+			bw.write(address.getHostAddress() + "\n");
+			bw.newLine();
+			bw.flush();
+		
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+
+		return address.getHostAddress();
+	}
+
+	public String url_send() {
+
+		return url;
+	}
+
+	public String url1_send() {
+
+		return url1;
+	}
+}
